7a90c6704254999cb96b0d9a6ae54ae9f2a71ca5
kherbig <kyleherbig@gmail.com>
2025-12-26 07:28:17 -0700

    security module

--- main.tf
+++ main.tf
@@ -36,11 +36,11 @@ module "blog_sg" {
 
   vpc_id      = data.aws_vpc.default.id
 
-  ingress_rules            = ["http-80-tcp", "https-80-tcp"]
-  ingress_with_cidr_blocks = ["0.0.0.0/0"]
+  ingress_rules       = ["http-80-tcp", "https-80-tcp"]
+  ingress_cidr_blocks = ["0.0.0.0/0"]
 
-  egress_rules             = ["all-all"]
-  egress_with_cidr_blocks  = ["0.0.0.0/0"]
+  egress_rules        = ["all-all"]
+  egress_cidr_blocks  = ["0.0.0.0/0"]
 }
 
 resource "aws_security_group" "blog" {
